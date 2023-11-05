432c12d164ea21ad1c3d57e47bdf0eec49c4a746
Sam Cartford (MBP) <cartford@hey.com>
2023-11-05 16:25:18 -0600

    loaded env file, deprecated exposed key lol

--- prices.py
+++ prices.py
@@ -1,71 +1,31 @@
 
 import datetime
-from polygon import RESTClient
-from polygon.rest.models import TickerSnapshot as PolygonTickerSnapshot, Agg
+from dotenv import load_dotenv
 from typing import Iterator, Optional, Union, List
+import os
 import pytz
+import requests
 
-from schemas import Baseline
-
-polygon_client = RESTClient(api_key="0Gc1iUzgI9N5IrAT4fXpq8pLQpvrLEJr")
-
-class PriceDay(Baseline):
-    avg_price: float
-    tot_volume: int
-    tick_dt: datetime.datetime
-    num_price_ticks: Optional[int] = None
-    open_price: Optional[float] = None
-    close_price: Optional[float] = None
+from schemas import TickerPricesResponse, PriceAgg
+load_dotenv(".env.shared")
 
 
-def get_ticker_daily_prices(
-        tick: str, 
-        start_day: datetime.datetime=datetime.datetime.today(), 
-        end_day: datetime.datetime=datetime.datetime.today()
-    ) -> Union[List[PriceDay], None]:
-    start_date_str = start_day.strftime("%Y-%m-%d")
-    end_date_str = end_day.strftime("%Y-%m-%d")
-
-    res = polygon_client.list_aggs(
-        ticker=tick,
-        multiplier=1,
-        timespan="day",
-        from_=start_date_str,
-        to=end_date_str
-    )
-    if isinstance(res, Iterator):
-        try:
-            all_aggs = [x for x in res]
-        except KeyError:
-            print(f"[polygon_error] {tick}: {start_day.isoformat()} <> {end_day.isoformat()}")
-            return None
-
-        found_prices = []
-        for agg in all_aggs:
-            timestamp_time = datetime.datetime.fromtimestamp(agg.timestamp / (10**3), tz=pytz.utc)
-
-            price_obj = PriceDay(
-                avg_price=agg.vwap,
-                tot_volume=int(agg.volume),
-                tick_dt=timestamp_time,
-                num_price_ticks=1,
-                open_price=agg.open,
-                close_price=agg.close
-            )
-            found_prices.append(price_obj)
-        return found_prices
-    
-    print(type(res))
-    print(res)
-    return None
+def get_ticker_daily_prices(ticker: str, start_dt: datetime.datetime, end_dt: datetime.datetime):
+    arg_str=f"?since={start_dt.isoformat()}&until={end_dt.isoformat()}"
+    api_resp = requests.get(os.environ.get("PROD_UPTRENDS_BASE_API", "https://api.uptrends.ai")+f"/stocks/{ticker.upper()}/prices{arg_str}")
+    prices_resp = api_resp.json()
+    prices_obj = TickerPricesResponse.model_validate(prices_resp)
+    return prices_obj.data
 
 
 def get_ticker_price_change_percent(ticker: str, start_dt: datetime.datetime, end_dt: datetime.datetime):
-    price_objs = get_ticker_daily_prices(tick=ticker, start_day=start_dt, end_day=end_dt)
+    #price_objs = get_ticker_daily_prices(tick=ticker, start_day=start_dt, end_day=end_dt)
+    price_objs: List[PriceAgg] = get_ticker_daily_prices(ticker, start_dt, end_dt)
+
     if price_objs is None or len(price_objs) < 2:
         return 0.0
     
-    prices_asc = sorted(price_objs, key=lambda x: x.tick_dt)
+    prices_asc = sorted(price_objs, key=lambda x: x.tick_datetime)
     open_price = prices_asc[0].open_price
     close_price = prices_asc[-1].close_price
     return (close_price - open_price) / open_price
